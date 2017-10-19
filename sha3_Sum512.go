043848dd16ac1fb4cd2c04d86e85143a99b34410
Rasputin <webmaster@katzen-in-ketchup-tauchen.de>
2017-10-19 19:37:14 +0200

    Überarbeitung

---
+++ sha3_Sum512.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/crypto/sha3"
+)
+
+type message struct {
+	plaintext string
+}
+
+func main() {
+	f1 := message{
+		"Ich bin Root! Ich darf das!",
+	}
+	f2 := message{
+		"Hello World!",
+	}
+	hashes := map[string][64]byte{
+		f1.plaintext: sha3.Sum512([]byte(f1.plaintext)),
+		f2.plaintext: sha3.Sum512([]byte(f2.plaintext)),
+	}
+	for k, v := range hashes {
+		fmt.Printf("k:\t%s\n", k)
+		fmt.Printf("v:\t%x\n", v)
+
+	}
+}
